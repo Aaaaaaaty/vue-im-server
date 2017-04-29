00184ef2f2e188e6b3a359347e544c6b7908acbe
幕希Lin <2103887953@qq.com>
2017-04-29 13:29:48 +0800

    login update

--- modules/userHandler.js
+++ modules/userHandler.js
@@ -5,49 +5,19 @@ var userHandler = {
     if(!user.username || !user.password) {
       return callback({
         status: 'ERROR',
-        data: {
-          user: null
-        },
+        data: { user: null },
         msg: '请输入用户名密码'
       })
     } else {
       User.get(user.username, function(err, u) {
         if(err) {
-          return callback({
-            status: 'ERROR',
-            meta: 'user',
-            data: {
-              user: null
-            },
-            msg: '服务器出错'
-          })
+          return callback({status: 'ERROR',meta: 'user',data: {user: null},msg: '服务器出错'})
         } else if (!u) {
-          return callback({
-            status: 'ERROR',
-            meta: 'user',
-            data: {
-              user: null
-            },
-            msg: '没有该用户'
-          })
+          return callback({status: 'ERROR',meta: 'user',data: {user: null},msg: '没有该用户'})
         } else if (u.password !== user.password) {
-          return callback({
-            status: 'ERROR',
-            meta: 'user',
-            data: {
-              user: null
-            },
-            msg: '请输入正确密码'
-          })
+          return callback({status: 'ERROR',meta: 'user',data: {user: null},msg: '请输入正确密码'})
         } else {
-          return callback({
-            status: 'OK',
-            meta: 'user',
-            data: {
-              user: u
-            },
-            msg: '登陆验证成功'
-          })
+          return callback({status: 'OK',meta: 'user',data: {user: u},msg: '登陆验证成功'})
         }
       })
     }
